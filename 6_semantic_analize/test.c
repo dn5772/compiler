c7128acc18a38e4723bdb16a96c2167c2588c45a
dn5772 <shendanny@naver.com>
2024-06-09 21:11:31 +0900

    update

--- 6_semantic_analize/test.c
+++ 6_semantic_analize/test.c
@@ -1,19 +1,26 @@
 int i;
 struct s { int a; struct x{int y; struct s g;} b; int c; } d;
 
-// enum t {
-// 	a,
-// 	b, 
-// 	c = 23,
-// 	d,
-// 	e = 'e',
-// };
+enum t {
+	a,
+	b, 
+	c = 23,
+	d,
+	e = 'e',
+};
+
+float fun (int a, int b){
+	int c,d;
+	{int e,f;}
+}
 
 int main(){
 	// i = 10;
 	int a[10];
 	int b[20];
 	a[0] = b[10];
-
+	float *fp;
+	int *ip;
+	fp=ip;
 	return 0;
 }
