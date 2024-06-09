0aa8e89dbb424e465e57588097ff638c12bf7de0
dn5772 <shendanny@naver.com>
2024-06-09 20:39:25 +0900

    add test.c

--- 6_semantic_analize/test.c
+++ 6_semantic_analize/test.c
@@ -11,14 +11,10 @@ struct s { int a; struct x{int y; struct s g;} b; int c; } d;
 
 int main(){
 	// i = 10;
-
 	int a[10];
 	int b[20];
-	
 	a[0] = b[10];
-
 	float *fp1;
 	int *ip;
-
 	return 0;
 }
